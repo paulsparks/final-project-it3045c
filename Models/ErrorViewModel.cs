c9ad7023602d130085c719c8c7f5c6783e19efc8
paulsparks <psparks1225@gmail.com>
2023-11-27 20:04:17 -0500

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace final_project_it3045c.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
