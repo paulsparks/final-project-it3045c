6ec334721774d969dd7059a3f6bf7aad8b7142aa
paulsparks <psparks1225@gmail.com>
2023-11-27 20:36:59 -0500

    add team member model

---
+++ Models/TeamMember.cs
@@ -0,0 +1,11 @@
+namespace final_project_it3045c.Models
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+        // Team member full name
+        // Birthdate (datetime)
+        // College Program
+        // Year in program: Freshman, Sophomore, etc.
+    }
+}
