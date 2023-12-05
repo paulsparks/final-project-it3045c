8bbb815730f88f7fb1531bb6b2cb0ac57d87c5d8
paulsparks <psparks1225@gmail.com>
2023-12-05 17:52:17 -0500

    make migration

---
+++ Migrations/20231205233343_Animals.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace final_project_it3045c.Migrations
+{
+    /// <inheritdoc />
+    public partial class Animals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Animals",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LifeSpan",
+                table: "Animals",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Animals",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Animals");
+
+            migrationBuilder.DropColumn(
+                name: "LifeSpan",
+                table: "Animals");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Animals");
+        }
+    }
+}
